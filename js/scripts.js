d0c708713683df1a678cbb619ddf78f6f04220b2
jonatan <ext_jsanroma@mercadolibre.com>
2023-10-20 01:56:27 -0300

    Se agrega nuevo item en el menu que muestra la ubicacion en google maps

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,24 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
 //FIN Menu
+
+function initMap() {
+    var geocoder = new google.maps.Geocoder();
+    var direccion = "DMF, Av. 12 de Octubre 1168, B1828 Villa Centenario, Provincia de Buenos Aires";
+    var mapa = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 15 // Nivel de zoom
+    });
+
+    geocoder.geocode({'address': direccion}, function(results, status) {
+      if (status === 'OK') {
+        mapa.setCenter(results[0].geometry.location); // Centra el mapa en las coordenadas del lugar
+        var marcador = new google.maps.Marker({
+          map: mapa,
+          position: results[0].geometry.location,
+          title: direccion // Nombre del lugar que se mostrará al hacer clic en el marcador
+        });
+      } else {
+        alert('La geocodificación no pudo ser completada debido a: ' + status);
+      }
+    });
+  }
